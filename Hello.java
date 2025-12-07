23c6d30d8636cecd402b98e1ee71b0226cb89635
Pamindu-Dasun <ict2022055@as.rjt.ac.lk>
2025-12-07 12:09:33 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+    
+    public static void printName(String name){
+        System.out.println("Welcome my github session "+ name );
+    }
 }
