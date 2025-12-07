28dab777fec314a66ba7f6d1c26e6f2191a6d896
Tharinda Pamindu <tharindapamindu@gmail.com>
2025-12-07 09:43:02 +0530

    Add StarPattern.cpp to the project

---
+++ StarPattern.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void starPatternOne(int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    starPatternOne(5);
+
+    return EXIT_SUCCESS;
+}
